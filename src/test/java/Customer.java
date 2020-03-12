1b118fe965c4cfedeec97e6c51f995fb181bcccb
Jignesh22 <jignesh.patel@radixweb.com>
2020-03-12 14:45:51 +0530

    Commit-1: Artif2 project creation.

---
+++ src/test/java/Customer.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class Customer {
+	
+	@Test
+	public void customer_1_Details() {
+		System.out.println("Customer-1:");
+		System.out.println("CustomerName: Abhinandan Sapkal"+"\n");
+	}
+	@Test
+	public void customer_2_Details() {
+		System.out.println("Customer-2:");
+		System.out.println("CustomerName: Amit Sharma"+"\n");
+	}
+	@Test
+	public void customer_3_Details() {
+		System.out.println("Customer-3:");
+		System.out.println("CustomerName: Jignesh Patel"+"\n");
+	}
+	
+	
+
+}
