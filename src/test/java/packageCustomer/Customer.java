b311f5a4a90005d184bd80451d19e57ef1e2262a
Jignesh22 <jignesh.patel@radixweb.com>
2020-03-13 13:37:51 +0530

    Commit March13 13:37 PM --> Commiting selenium java code

--- src/test/java/packageCustomer/Customer.java
+++ src/test/java/packageCustomer/Customer.java
@@ -1,27 +1,37 @@
 package packageCustomer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Customer {
 	
-	@Test
-	public void customer_1_Details() {
-		System.out.println("Customer-1:");
-		System.out.println("CustomerName: Abhinandan Sapkal"+"\n");
-		//Assert.fail();
-	}
-	@Test
-	public void customer_2_Details() {
-		System.out.println("Customer-2:");
-		System.out.println("CustomerName: Amit Sharma"+"\n");
-	}
-	@Test
-	public void customer_3_Details() {
-		System.out.println("Customer-3:");
-		System.out.println("CustomerName: Jignesh Patel"+"\n");
-		
+	public WebDriver driver;
+	
+	@Test(priority=1)
+	public void launchBrowserAndOpenURL() {
+		//launching chrome browser
+		System.out.println("Run1: launching firefox browser"); 
+	    System.setProperty("webdriver.gecko.driver", "./chromedriver");
+	    driver = new ChromeDriver();	  
+	    //Opening URL
+		System.out.println("Run1: Opening url"); 
+	    driver.get("http://opsqa.radixdev68.com/admin");
 	}
 	
+	@Test(priority=2)
+	public void performLogin() {
+		System.out.println("Run2: Logging to the OPS"); 
+		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("admin");
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("admin");
+		driver.findElement(By.xpath(".//*[@type='submit']")).click();
+		System.out.println("Run2: Logged in successful to the OPS"); 
+	}
 	
-
+	@Test(priority=3)
+	public void closingBrowser() {
+		driver.close();
+		System.out.println("Run3: Browser Closed"); 
+	}
 }
