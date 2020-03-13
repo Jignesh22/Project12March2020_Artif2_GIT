5cdb23c95e10939f96515f1704becf1b098b3b60
Jignesh22 <jignesh.patel@radixweb.com>
2020-03-13 13:46:20 +0530

    Commit March13 13:45 PM ---> Modification in selenium code[added waits]

--- src/test/java/packageCustomer/Customer.java
+++ src/test/java/packageCustomer/Customer.java
@@ -10,23 +10,26 @@ public class Customer {
 	public WebDriver driver;
 	
 	@Test(priority=1)
-	public void launchBrowserAndOpenURL() {
+	public void launchBrowserAndOpenURL() throws InterruptedException {
 		//launching chrome browser
 		System.out.println("Run1: launching firefox browser"); 
 	    System.setProperty("webdriver.gecko.driver", "./chromedriver");
-	    driver = new ChromeDriver();	  
+	    driver = new ChromeDriver();	
+	    Thread.sleep(2000);
 	    //Opening URL
 		System.out.println("Run1: Opening url"); 
 	    driver.get("http://opsqa.radixdev68.com/admin");
+	    Thread.sleep(2000);
 	}
 	
 	@Test(priority=2)
-	public void performLogin() {
+	public void performLogin() throws InterruptedException {
 		System.out.println("Run2: Logging to the OPS"); 
 		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("admin");
 		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("admin");
 		driver.findElement(By.xpath(".//*[@type='submit']")).click();
 		System.out.println("Run2: Logged in successful to the OPS"); 
+	    Thread.sleep(2000);
 	}
 	
 	@Test(priority=3)
