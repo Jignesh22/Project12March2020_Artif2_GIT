bd5a641d132aa6fde71acd03d1e4e349fa6224ed
Jignesh22 <jignesh.patel@radixweb.com>
2020-03-17 16:33:09 +0530

    Commit: Bitbucket updated with chrome standalone

--- src/test/java/packageCustomer/Customer.java
+++ src/test/java/packageCustomer/Customer.java
@@ -1,9 +1,13 @@
 package packageCustomer;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,16 +16,22 @@ public class Customer {
 	public WebDriver driver;
 	
 	@Test(priority=1)
-	public void launchBrowserAndOpenURL() throws InterruptedException {
+	public void launchBrowserAndOpenURL() throws InterruptedException, MalformedURLException {
 		//launching chrome browser
 		System.out.println("Run1: launching chrome browser"); 
-	    System.setProperty("webdriver.chrome.driver", "./chromedriver");
-	    driver = new ChromeDriver();	
 		
+		URL remoteURL = new URL("http://127.0.0.1:4444/wd/hub");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		driver = new RemoteWebDriver(remoteURL, options);
+//	    System.setProperty("webdriver.chrome.driver", "./chromedriver");
+//	    driver = new ChromeDriver();
 	    Thread.sleep(2000);
+	    
 	    //Opening URL
-		System.out.println("Run1: Opening url"); 
+		System.out.println("Run1: Pass1"); 
 	    driver.get("http://opsqa.radixdev68.com/admin");
+		System.out.println("Run1: Pass2"); 
 	    Thread.sleep(2000);
 	}
 	
