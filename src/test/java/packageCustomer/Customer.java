d494a2dfa5d147ad120c043ca30c35eaa7e329e5
Jignesh22 <jignesh.patel@radixweb.com>
2020-03-24 13:00:37 +0530

    Commit: Running browser launch basic script

--- src/test/java/packageCustomer/Customer.java
+++ src/test/java/packageCustomer/Customer.java
@@ -18,45 +18,23 @@ public class Customer {
 	@Test(priority=1)
 	public void launchBrowserAndOpenURL() throws InterruptedException, MalformedURLException {
 		//launching chrome browser
-		System.out.println("Run1: launching chrome browser"); 
-		
-
-//Run remotely inside docker container	
+		System.out.println("RUN-1: launching chrome browser"); 
 
 	//	URL remoteURL = new URL("http://127.0.0.1:4444/wd/hub");
-		ChromeOptions options = new ChromeOptions();
-		options.setBinary("/usr/bin/google-chrome");
+	//	ChromeOptions options = new ChromeOptions();
+	//	options.setBinary("/usr/bin/google-chrome");
 	//	driver = new RemoteWebDriver(remoteURL, options);
-	
 	//    System.setProperty("webdriver.chrome.driver", "./chromedriver");
-	    driver = new ChromeDriver(options);
+
+		driver = new ChromeDriver();
 	    Thread.sleep(2000);
-	    
-	    
-	    
+	     
 	    //Opening URL
-		System.out.println("Run1: Pass1"); 
+		System.out.println("RUN-2: Chrome Launched"); 
 	    driver.get("http://opsqa.radixdev68.com/admin");
-		System.out.println("Run1: Pass2"); 
+		System.out.println("Run-3: URL opened"); 
 	    Thread.sleep(2000);
 	}
-	
-	@Test(priority=2)
-	public void performLogin() throws InterruptedException {
-		System.out.println("Run2: Logging to the OPS"); 
-		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("admin");
-		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("admin");
-		driver.findElement(By.xpath(".//*[@type='submit']")).click();
-		System.out.println("Run2: Logged in successful to the OPS"); 
-	    Thread.sleep(2000);
-	}
-	
-	@Test(priority=3)
-	public void closingBrowser() {
-		driver.close();
-		System.out.println("Run3: Browser Closed"); 
-	}
-	//CommitForCheckingPullRequest
-	//DemoCheckWorkingFromHome
+
 
 }
