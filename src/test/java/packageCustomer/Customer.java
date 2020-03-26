09523bacbe2eb9aaaed7458ec3904a3cd1bc8bec
PREM <PREM@DESKTOP-ES79AE3>
2020-03-26 11:56:39 +0530

    Commit: working from windows

--- src/test/java/packageCustomer/Customer.java
+++ src/test/java/packageCustomer/Customer.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class Customer {
 	
@@ -19,16 +19,17 @@ public class Customer {
 	public void launchBrowserAndOpenURL() throws InterruptedException, MalformedURLException {
 		//launching chrome browser
 		System.out.println("RUN-1: launching chrome browser"); 
-	    System.setProperty("webdriver.chrome.driver", "./chromedriver");
+	    System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
 		System.out.println("RUN-2"); 
 	//	URL remoteURL = new URL("http://127.0.0.1:4444/wd/hub");
-		ChromeOptions options = new ChromeOptions();
+	//	ChromeOptions options = new ChromeOptions();
 		System.out.println("RUN-3"); 
 	//	options.setBinary("/usr/bin/google-chrome");
-		options.addArguments("headless");
-		driver = new ChromeDriver(options);
+	//	options.addArguments("headless");
+		driver = new ChromeDriver();
 	    Thread.sleep(2000);
 	     
+	    
 	    
 	    //Opening URL
 		System.out.println("RUN-4: Chrome Launched"); 
